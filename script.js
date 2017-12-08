7ee2eca44794a73858b4f61e8264eaa358b5964b
Cornalia Steinkelner <cornelia.steinkelner@chello.at>
2017-12-08 14:07:46 +0100

    added Event Click

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var horse_power = 250;
 
 function calculateInsurance(Name,Age,Country,Horse_power){
 	var insurance = Horse_power * 100 / Age + 150;
-	return name + ", your Insurance would be " + insurance
+	
+	document.getElementById("result").innerHTML = name + ", your Insurance would be " + insurance
 };
-document.getElementById("result").innerHTML 
-= calculateInsurance(name,age,country,horse_power);
